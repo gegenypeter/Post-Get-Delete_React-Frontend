d70ba987019e36733db5263fbc919d7ca198a9c6
GegenyPeter <gegeny.p@gmail.com>
2022-02-09 20:04:25 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import http from 'axios';
+
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [characters, setCharacters] = useState("")
+  const [serieses, setSerieses] = useState([]);
+
+  const create = async () => {
+    await http.post("http://localhost:3000/api/serieses", {
+      name: title,
+      charList: characters.split(',')
+    })
+    load();
+    setTitle("");
+    setCharacters("");
+  };
+
+  const del = async (sid) => {
+    await http.delete(`http://localhost:3000/api/serieses/${sid}`)
+    load()
+  };
+
+  const load = async () => {
+    const response = await http.get("http://localhost:3000/api/serieses")
+    setSerieses(response.data);
+  };
+
+  useEffect(() => {
+    load();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} ></input>
+      <textarea placeholder="characters" value={characters} onChange={(e) => setCharacters(e.target.value)} ></textarea>
+      <button onClick={() => create()}>Create</button>
+      {serieses.map(series => (
+        <div key={series.id}>
+          <p>{series.name}</p>
+          <ul>
+            {series.characters.map(character => (
+              <li key={character}>{character}</li>
+            )
+            )}
+          </ul>
+          <button onClick={() => del(series.id)} >Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
